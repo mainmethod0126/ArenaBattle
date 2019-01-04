66344d4cf6f1940471cc9bd71948c9a10c29743e
wusubsin <mainmethod0126@gmail.com>
2019-01-04 23:49:55 +0900

    Title(Type:Feat) KOR : ABGameMode, ABPalyerController, ABPawn 추가 및 폰에 무료 SkeletalMesh 적용
    Body
    KOR : ArenaBattle에서 Player의 로그인과 플레이어 컨트롤러, 폰 Possess를 담당할 ABGameMode를 추가,
    플레이어가 로그인 되었을 시 플레이어와 1:1로 소통하며 폰을 움직일 수 있는 ABPlayerController를 추가,
    플레이어가 움질일 수 있는 ABPawn 추가.
    멀티 플레이까지 고려한다면 게임에 몇 명의 플레이어가 들어올지 모르기 대문에, 미리 폰을 만들어두는 것이 아니라
    클래스 정보만 저장해놓고 플레이어가 입장할 때마다 해당 클래스 정보를 기반으로 폰을 생성할 수 있도록
    각 오브젝트에 존재하는 StaticClass 함수를 호출하여 오브젝트의 클래스 정보를 가져와서 등록.
    무료 애셋 스토어에서 재공하는 파라곤 애셋을 다운받아 ABPawn에 SkeletalMesh를 등록하고
    UCapsuleComponent : 폰의 움직임을 담당하는 충돌 컴포넌트
    USkeletalMeshComponent : 캐릭터 애셋을 보여주고 애니메이션을 담당하는 컴포넌트
    UFloatingPawnMovement : 플레이어의 입력에 따라 캐릭터가 움직이도록 설정해주는 컴포넌트 (중력을 고려하지 않음)
    USpringArmComponent : 삼인칭 시점으로 카메라 구도를 편리하게 설정할 수 있는 부가 컴포넌트
    UCameraComponent : 폰에 부착할 카메라 컴포넌트 스프링암 컴포넌트의 자식으로 설정하고 트랜스폼 정보를 초기화 하여 자동으로 스프링암 끝에 걸리도록 함
    위의 5개의 컴포넌트를 추가함

---
+++ Source/ArenaBattle/ABPawn.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ABPawn.h"
+
+// Sets default values
+AABPawn::AABPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CAPSULE"));
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MESH"));
+	Movement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("MOVEMENT"));
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SPRINGGARM"));
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("CAMERA"));
+
+	RootComponent = Capsule;
+	Mesh->SetupAttachment(Capsule);
+	SpringArm->SetupAttachment(Capsule);
+	Camera->SetupAttachment(SpringArm);
+
+	Capsule->SetCapsuleHalfHeight(120.0f);
+	Capsule->SetCapsuleRadius(34.0f);
+	Mesh->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -88.0f), FRotator(0.0f, -90.0f, 0.0f));
+	SpringArm->TargetArmLength = 400.0f;
+	SpringArm->SetRelativeRotation(FRotator(-15.0f, 0.0f, 0.0f));
+
+	FString strSkeletalMeshPath = TEXT("");
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SK_CARDBOARD(TEXT("/Game/ParagonGreystone/Characters/Heroes/Greystone/Meshes/Greystone.Greystone"));
+	
+	if (SK_CARDBOARD.Succeeded())
+	{
+		Mesh->SetSkeletalMesh(SK_CARDBOARD.Object);
+	}
+}
+
+// Called when the game starts or when spawned
+void AABPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AABPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AABPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
